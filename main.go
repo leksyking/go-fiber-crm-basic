e9404f4658910139bfb49a8bfd0fd25b0c3f2930
Leksyking <gbemilekeogundipe@gmail.com>
2022-04-21 18:19:21 -0700

    set up routes with go fiber and start server

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"github.com/gofiber/fiber"
+)
 
+func setUpRoutes(app *fiber.App) {
+	app.Get(GetLeads)
+	app.GetO(GetLead)
+	app.Post(NewLead)
+	app.Delete(DeleteLead)
+}
+func main() {
+	app := fiber.New()
+	setUpRoutes(app)
+	app.Listen(3000)
 }
